a6d4a6e64ddd316fea3680166ee67959855616fc
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-05 21:31:23 +0100

    Create constr.R

---
+++ squadP/R/constr.R
@@ -0,0 +1,9 @@
+#' @title .
+#' @description \code{constr} .
+#' @usage constr(x)
+#' @param x 
+#' @details 
+#' @return 
+#' @references Work in progress.
+#' @author Adam Bekhit, Jakob Schöpe
+#' @export
