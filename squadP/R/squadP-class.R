ca467f14ed9a1b580db26a2fde34dbe55c386000
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-06 00:18:27 +0100

    Update squadP-class.R

--- squadP/R/squadP-class.R
+++ squadP/R/squadP-class.R
@@ -1,12 +1,14 @@
 #' @title S4 Class \code{"squadP"}
 #' @slot call .
+#' @slot formula An object of class \code{"formula"}.
 #' @slot coefficients A numerical vector containing the estimated coefficients of the log-binomial model.
 #' @slot iter A positive integer indicating the number of iterations.
 #' @slot converged A logical constant that indicates whether the log-binomial model has converged.
 #' @slot y A numerical vector containing the dependent variable of the log-binomial model.
 #' @slot x The model matrix of the log-binomial model.
+#' @slot data A data frame containing the variables in the model.
 #' @author Adam Bekhit, Jakob Schöpe
 #' @import Matrix matrixStats quadprog
 #' @export
 
-setClass(Class = "squadP", slots = c(call = "language", coefficients = "numeric", iter = "numeric", converged = "logical", y = "numeric", x = "matrix"))
+setClass(Class = "squadP", slots = c(call = "language", formula = "formula", coefficients = "numeric", iter = "numeric", converged = "logical", y = "numeric", x = "matrix", data = "data.frame"))
