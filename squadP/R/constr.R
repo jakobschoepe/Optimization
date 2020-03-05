535fa9defa74c83fcd0c6228bc8ea5f8bac3ff7b
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-05 21:31:50 +0100

    Update constr.R

--- squadP/R/constr.R
+++ squadP/R/constr.R
@@ -7,3 +7,11 @@
 #' @references Work in progress.
 #' @author Adam Bekhit, Jakob Schöpe
 #' @export
+
+constr <- function(x) {
+  colMax <- matrixStats::colMaxs(x)
+  colMin <- matrixStats::colMins(x)
+  const <- expand.grid(lapply(2:length(colMax), function(i) {c(colMin[i], colMax[i])}))
+  Amat <- unname(as.matrix(cbind(rep(1, times = nrow(const)), const)))
+  return(-Amat)
+}
