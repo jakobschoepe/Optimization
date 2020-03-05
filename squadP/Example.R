499bc4d50264895b1651bcaebf92d01631283149
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-05 22:18:26 +0100

    Update and rename squadP/R/Example.R to squadP/Example.R

--- squadP/Example.R
+++ squadP/Example.R
@@ -1,4 +1,6 @@
+setwd("~/Downloads/num-optim-master/squadP/R")
 sapply(list.files(pattern = "*.R"), source, .GlobalEnv)
+set.seed(123)
 myDF <- data.frame(y = sample(c("no", "yes"), size = 150, replace = TRUE, prob = c(0.90, 0.10)), x1 = rnorm(150))
 fit <- squadP(formula = y ~ x1, data = myDF)
 summary(fit)
