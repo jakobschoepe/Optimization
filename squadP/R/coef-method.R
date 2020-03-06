9fa894047c90ef23f404bb3708b00d5e4c637729
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-06 12:51:12 +0100

    Update coef-method.R

--- squadP/R/coef-method.R
+++ squadP/R/coef-method.R
@@ -2,7 +2,6 @@
 #' @description For objects of class \code{"squadP"}, \code{coef} extracts the estimated model parameters of the fitted log-binomial model.
 #' @param object An object of class \code{"squadP"}.
 #' @return A real vector giving the estimated model parameters.
-#' @references Work in progress.
 #' @author Adam Bekhit, Jakob Schöpe
 #' @export
 
