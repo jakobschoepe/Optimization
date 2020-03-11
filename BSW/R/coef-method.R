5aaaf2a1b165e94be405266ba75b7379beea2436
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-11 15:37:17 +0100

    Update coef-method.R

--- BSW/R/coef-method.R
+++ BSW/R/coef-method.R
@@ -1,5 +1,6 @@
 #' @title Extracting the estimated model parameters of \code{bsw}
 #' @description For objects of class \code{"bsw"}, \code{coef} extracts the estimated model parameters of \code{bsw}.
+#' @usage coef(object)
 #' @param object An object of class \code{"bsw"}.
 #' @return A numeric vector giving the estimated model parameters.
 #' @author Adam Bekhit, Jakob Schöpe
