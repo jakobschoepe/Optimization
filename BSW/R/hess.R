4ce8db0b8b1f4ed82bc24d21056aa853b1e16387
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-11 15:05:55 +0100

    Update hess.R

--- BSW/R/hess.R
+++ BSW/R/hess.R
@@ -1,10 +1,10 @@
-#' @title Computing the Hessian matrix in \code{bsw}
-#' @description \code{hess} is a helper function for \code{bsw} to compute the Hessian matrix.
+#' @title Deriving the second partial derivatives of the likelihood function in \code{bsw} (Hessian matrix)
+#' @description \code{hess} is a helper function for \code{bsw} to derive the second partial derivatives of the likelihood function.
 #' @usage hess(theta, y, x)
 #' @param theta A numeric vector containing the model parameters.
 #' @param y A numeric vector containing the dependent variable of the model.
 #' @param x The model matrix.
-#' @return The Hessian matrix in \code{bsw}.
+#' @return A numeric matrix containing the second partial derivatives of the likelihood function (Hessian matrix).
 #' @author Adam Bekhit, Jakob Schöpe
 #' @export
 
