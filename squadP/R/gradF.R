3daf4c8ac1b50bfc319580b9086bf4ecb3fc81a8
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-11 14:52:10 +0100

    Update gradF.R

--- squadP/R/gradF.R
+++ squadP/R/gradF.R
@@ -1,9 +1,9 @@
-#' @title Deriving the first derivative of the likelihood function in \code{squadP}
-#' @description \code{gradF} is a helper function for \code{squadP} to derive the first derivative of the likelihood function.
+#' @title Deriving the first derivative of the likelihood function in \code{bsw}
+#' @description \code{gradF} is a helper function for \code{bsw} to derive the first derivative of the likelihood function.
 #' @usage gradF(theta, y, x)
 #' @param theta A numeric vector containing .
-#' @param y A numeric vector containing the dependent variable of the log-binomial model.
-#' @param x The model matrix of the log-binomial model.
+#' @param y A numeric vector containing the dependent variable of the model.
+#' @param x The model matrix.
 #' @return A numeric vector containing the first derivatives of the likelihood function.
 #' @author Adam Bekhit, Jakob Schöpe
 #' @export
