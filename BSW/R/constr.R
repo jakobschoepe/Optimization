8525d9922595b76e32d980c93cbfff98b122fbbf
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-13 14:31:28 +0100

    Update constr.R

--- BSW/R/constr.R
+++ BSW/R/constr.R
@@ -1,8 +1,8 @@
-#' @title Setting the linear inequality constraints for \code{bsw}
-#' @description \code{constr} sets the linear inequality constraints for \code{bsw}.
+#' @title Setting the linear inequality constraints for \code{bsw()}
+#' @description \code{constr()} sets the linear inequality constraints for \code{bsw()}.
 #' @usage constr(x)
 #' @param x A model matrix.
-#' @return A matrix containing the linear inequality constraints for \code{bsw}.
+#' @return A matrix containing the linear inequality constraints for \code{bsw()}.
 #' @author Adam Bekhit, Jakob Schöpe
 #' @export
 
